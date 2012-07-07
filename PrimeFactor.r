9202d5eeeaebbdb4929da7bb3a8bf03ad66cdc20
tonyworld <tong.nicholwei@gmail.com>
2012-07-07 18:33:46 +0800

    PrimeFactor

---
+++ PrimeFactor.r
@@ -0,0 +1,21 @@
+## to factorize an integar number into prime numbers
+
+PrimeFactor <- function(x, prime = prime) {
+  m <- length(prime)
+  fac.count <- numeric(m)
+  names(fac.count) <- prime
+  # actually, a primality check could insert here
+  for (i in 1:m) {
+    prime.num <- prime[i]
+    if (x %% prime.num == 0) {
+      fac.count[i] <- 1
+      x = x / prime.num
+      while (x %% prime.num == 0) {
+        fac.count[i] <- fac.count[i] + 1
+        x = x / prime.num
+	    while (x == 1) break
+	  }
+    }  
+  }
+  return(fac.count)
+}
